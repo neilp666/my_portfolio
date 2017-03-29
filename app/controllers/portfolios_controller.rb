e6a195a9c93393422201cbeabab35ac190f5beaa
neil patel <neil@myindianweddingapp.com>
2017-03-29 16:17:11 +0100

    Added new and create actions

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -4,6 +4,22 @@ class PortfoliosController < ApplicationController
     @portfolio_items = Portfolio.all
   end
 
+  def new
+   @portfolio_item = Portfolio.new
+  end
+
+  def create
+   @portfolio_item = Portfolio.new(portfolio_params)
+
+   respond_to do |format|
+    if @portfolio_item.save
+      format.html { redirect_to @portfolios_path, notice: "Your portfolio item is now live"}
+    else
+      format.html { render :new}
+    end
+   end
+  end
+
   def update
   end
 
@@ -11,5 +27,9 @@ class PortfoliosController < ApplicationController
   end
 
   private
+
+  def portfolio_params
+    params.require(:portfolio_item).permit(:title, :subtitle, :body, :main_image, :thumb_image)
+  end
 
 end
