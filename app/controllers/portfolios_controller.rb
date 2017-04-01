f4802d112fb657780800fa7dcbb703ec95ccfb4a
neil patel <neil@myindianweddingapp.com>
2017-04-01 16:46:14 +0100

    Added new edit buttons for portfolio items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -8,6 +8,10 @@ class PortfoliosController < ApplicationController
    @portfolio_item = Portfolio.new
   end
 
+  def edit
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
   def create
    @portfolio_item = Portfolio.new(portfolio_params)
 
@@ -21,6 +25,13 @@ class PortfoliosController < ApplicationController
   end
 
   def update
+    respond_to do |format|
+      if @portfolio_item.update(portfolio_params)
+        format.html { redirect_to portfolios_path, notice: 'The record was  successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
   end
 
   def destroy
