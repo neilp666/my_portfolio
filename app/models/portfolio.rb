51decb75623fcfe5b7914bf062fd5fda47a7c5f4
neil patel <neil@myindianweddingapp.com>
2017-04-04 16:35:42 +0100

    Implemented concern to manage image

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,16 +1,17 @@
 class Portfolio < ApplicationRecord
+  include Placeholder
   validates_presence_of :title, :body, :main_image, :thumb_image
 
   def self.angular
     where(subtitle: 'Angular')
   end
 
-  scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails')}
+  scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails') }
 
   after_initialize :set_defaults
 
   def set_defaults
-    self.main_image ||= "http://plachold.it/600x400"
-    self.thumb_image ||= "http://plachold.it/350x200"
+    self.main_image ||= Placeholder.image_generator(height: '600', width: '4000')
+    self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
   end
 end
