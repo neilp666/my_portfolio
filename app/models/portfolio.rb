f80bd1acc94a21fb259b290c7832776457d3e406
neil patel <neil@myindianweddingapp.com>
2017-04-04 16:54:31 +0100

    Updated portfolio model and intergrated technologies model

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,5 +1,6 @@
 class Portfolio < ApplicationRecord
   include Placeholder
+  has_many :technologies
   validates_presence_of :title, :body, :main_image, :thumb_image
 
   def self.angular
@@ -9,6 +10,8 @@ class Portfolio < ApplicationRecord
   scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails') }
 
   after_initialize :set_defaults
+
+
 
   def set_defaults
     self.main_image ||= Placeholder.image_generator(height: '600', width: '4000')
